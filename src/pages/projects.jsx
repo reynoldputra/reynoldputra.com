bb465b7aab3a6fbb2ce56fd237ac5ff941c4c219
reynoldputra <reynoldputra1@gmail.com>
2023-05-02 14:41:35 +0700

    fix: build error

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -3,6 +3,7 @@ import Layout from "../components/general/layout";
 import ProjectSection from "../components/projects/projectSection";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import Loading from "../components/general/loading";
 
 export default function Projects() {
   const [isLoading, setLoading] = useState(false)

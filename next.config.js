257f5c41f0bc7610df88d95405b28651461caa15
reynoldputra <reynoldputra1@gmail.com>
2023-06-03 14:12:56 +0700

    feat: add env on next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env : {
+    HOST : process.env.HOST,
+    SERVER_HOST : process.env.SERVER_HOST
+  }
 }
 
 module.exports = nextConfig
